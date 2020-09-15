5bd50e4f4a9f1fd0b904147b0ef6441af7bb81ac
rotocolo <romualdo.rijo@sigob.org>
2020-09-15 09:51:49 -0400

    Modificaciones para IOSx

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 
 importScripts("js/sw-util.js");
 
-const STATIC_CACHE = "static-v3";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v4";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
@@ -48,6 +48,11 @@ self.addEventListener("activate", e => {
 
             // static-v4
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+            // dynamic-v4
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
 

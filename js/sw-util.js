01f35457bcde878d4ffe58ba68cce27ed7616add
rotocolo <romualdo.rijo@sigob.org>
2020-09-14 23:03:45 -0400

    first commit

---
+++ js/sw-util.js
@@ -0,0 +1,17 @@
+﻿// Guardar en el cache dinamico
+function actualizarCacheDinamico(dynamicCache, req, res) {
+    if (res.ok) {
+
+        return caches.open(dynamicCache).then(cache => {
+
+            cache.put(req, res.clone());
+
+            return res.clone();
+
+        });
+    } else {
+
+        return res;
+
+    }
+}
